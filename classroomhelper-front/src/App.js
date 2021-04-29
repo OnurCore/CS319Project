596967b2a653de0207c465aafc0f30e658ffc35f
contactLost <50428210+contactLost@users.noreply.github.com>
2021-04-30 01:46:08 +0300

    merged all groups and navigational path
    All groups is in wrong place tho. Its on my group. Need to fix that

--- classroomhelper-front/src/App.js
+++ classroomhelper-front/src/App.js
@@ -1,20 +1,171 @@
-import GroupTable from "./GroupTable"
 
-function App() {
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import './App.css';
+import GroupTable from "./Groups/GroupTable"
 
-  //Consturctor
-
-  //Function
+import React from 'react';
 
 
+export default function BasicExample() {
   return (
-    <div className="App">
-      <GroupTable>
-        
+    <Router>
+      <div>
+          <Switch>
+
+            <Route exact path="/">
+                <App />
+            </Route>
+
+            <Route path="/home">
+                <Home />
+            </Route>
+          
+            <Route path="/mygroup">
+                <MyGroup />
+            </Route>
       
-      </GroupTable>
-    </div>
+              <Route path="/people">
+                <People />
+              </Route>
+
+          </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Classroom Helper
+          </p>
+
+          <form>
+            <label>
+              Username:
+              <input type="text" name="Username"/>
+            </label>
+          </form>
+
+          <form>
+            <label>
+              Password:
+              <input type="text" name="Password" />
+            </label>
+          </form>
+
+          <Link to="/Home">  <button>Login</button>   </Link>  
+
+          <button>Sign-up</button>
+        </header>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return (
+    <Router>
+        <div className="App">
+
+          <p>
+              Homepage
+          </p>
+      
+          <Link to="/home">       <button>Home</button>        </Link>
+          <Link to="/mygroup">    <button>My Groups</button>   </Link>
+          <Link to="/people">     <button>People</button>      </Link>     
+              
+          <button>All Groups</button>
+              
+          <button>Gradebook</button>
+      
+          <button>Info</button>
+ 
+        </div>
+        </Router>
+      );
+  
+}
+
+function MyGroup(){
+return (
+  <Router>
+      <div className="App">
+      
+        <h1>
+            My Group
+        </h1>
+
+
+        <Link to="/home"> <button>Home </button></Link>
+        <Link to="/mygroup"> <button> My Group </button></Link>
+        <Link to="/people"> <button> People </button></Link>     
+        
+        <button>All Groups</button>
+                
+        <button>Gradebook</button>
+            
+        <button>Info</button>
+      
+        <GroupTable>
+          </GroupTable>
+      </div>
+  </Router>
+  );
+}
+
+
+function People(){
+  return (
+    <Router>
+        <div className="App">
+          <p>
+              People
+          </p>
+    
+        <Link to="/home"> <button>Home </button></Link>
+        <Link to="/mygroup"> <button> My Group </button></Link>
+        <Link to="/people"> <button> People </button></Link>     
+        
+        <button>All Groups</button>
+                
+        <button>Gradebook</button>
+            
+        <button>Info</button>
+     
+        <p>
+        Participants List
+        </p>
+
+        <ul>
+          <li>
+            Funda Tan             
+            <button>
+            Send invitation
+            </button>   
+          </li>
+
+          <li>
+            Ezgi Saygılı           
+            <button>
+            Send invitation
+            </button>
+          </li>
+      </ul>
+        
+      </div>
+    </Router>
+  );
+}
+
+
+
